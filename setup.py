8926ae96ba12877294b9ae225aad02e72d4abcc0
Archanaa D <archanaadayalam@gmail.com>
2025-11-20 00:53:50 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="legalai_chatbot",
+    version="0.1.0",
+    author="Archanaa D",
+    author_email="archanaadayalam@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
